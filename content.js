b669227508e9050908a0a8457379d3dc41788835
Pirabarlen Cheenaramen <pc@crediwire.com>
2016-12-08 13:27:03 +0000

    add code in files types: .php remove code in files types: .php

--- content.js
+++ content.js
@@ -1,2 +1,3 @@
 console.log("ehuvs")
 console.log("piljy")
+console.log("cqkij")
--- content.php
+++ content.php
@@ -114,5 +114,5 @@
         echo "gdlur";
         echo "mreae";
         echo "kendo";
-        echo "sqvrt";
+        echo "lrxet";
 ?>
