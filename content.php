396060791a4cf6878106a95781baed15ee9d9022
Pirabarlen Cheenaramen <pc@crediwire.com>
2017-09-22 07:50:56 +0000

    add code in files types: .php remove code in files types: .php

--- content.php
+++ content.php
@@ -164,5 +164,5 @@
         echo "nkiwk";
         echo "hqono";
         echo "jffwh";
-        echo "ifmqh";
+        echo "ojehc";
 ?>
--- content.sql
+++ content.sql
@@ -2,4 +2,3 @@ SELECT * from wgiwq;
 SELECT * from ibbbl;
 SELECT * from opuew;
 SELECT * from qadeu;
-SELECT * from pcmjq;
