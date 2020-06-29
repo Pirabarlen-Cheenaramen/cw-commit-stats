c02e252d17d704f6c74750c9ae3dd6316e032aca
Pirabarlen Cheenaramen <pc@crediwire.com>
2020-06-29 06:40:58 +0000

    add code in files types: .sh,.php,.phtml,.sql remove code in files types: .sh,.php,.phtml,.sql

--- content.php
+++ content.php
@@ -357,5 +357,5 @@
         echo "ooxvn";
         echo "xgtso";
         echo "yrtax";
-        echo "wrlhu";
+        echo "jhchn";
 ?>
--- content.sql
+++ content.sql
@@ -14,4 +14,4 @@ SELECT * from vjjhp;
 SELECT * from bwegu;
 SELECT * from ptrwo;
 SELECT * from iyrnn;
-SELECT * from gcbhh;
+SELECT * from inxwt;
